bfae47f96b23b619d24b938ae8d46745178c30e3
mh <mh@immerda.ch>
2019-06-17 17:52:57 +0200

    add a storage account for the registry

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -8,3 +8,15 @@ resource "azurerm_storage_account" "ocp" {
       ignore_changes        = ["name"]
   }
 }
+
+resource "azurerm_storage_account" "registry" {
+  name                      = "registry${lower(replace(substr(uuid(), 0, 10), "-", ""))}"
+  resource_group_name       = "${data.azurerm_resource_group.ocp.name}"
+  location                  = "${data.azurerm_resource_group.ocp.location}"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  lifecycle {
+      ignore_changes        = ["name"]
+  }
+
+}
