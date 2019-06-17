396a62d35f6f234792a4050faa538dc6335a79d2
mh <mh@immerda.ch>
2019-06-17 17:55:00 +0200

    add service account for acme

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,3 +36,34 @@ resource "azurerm_role_assignment" "azure_app" {
   role_definition_name = "Contributor"
   principal_id         = "${azuread_service_principal.azure_app.id}"
 }
+
+resource "random_id" "acme_app" {
+  byte_length = 8
+  prefix      = "acme-${data.azurerm_resource_group.ocp.name}-"
+}
+
+resource "azuread_application" "acme_app" {
+  name                       = "${random_id.acme_app.hex}"
+  available_to_other_tenants = false
+}
+
+resource "azuread_service_principal" "acme_app" {
+  application_id = "${azuread_application.acme_app.application_id}"
+}
+
+resource "random_string" "acme_password" {
+  length  = 32
+  special = true
+}
+
+resource "azuread_service_principal_password" "acme_app" {
+  service_principal_id = "${azuread_service_principal.acme_app.id}"
+  value                = "${random_string.acme_password.result}"
+  end_date_relative    = "17520h"
+}
+
+resource "azurerm_role_assignment" "acme_app" {
+  scope                = "${data.azurerm_dns_zone.ocp.id}"
+  role_definition_name = "DNS Zone Contributor"
+  principal_id         = "${azuread_service_principal.acme_app.id}"
+}
